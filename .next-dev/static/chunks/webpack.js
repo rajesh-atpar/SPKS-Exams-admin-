fae80558a3443236f0a433f5df47a2d03e722d49
fusioncrafttech <fusioncrafttech@gmail.com>
2026-09-19 20:13:42 +0530

    refactor: clean up app paths and build manifest; update webpack hash
    - Removed unused entries for the not-found page from the app build manifest and app paths manifest.
    - Updated the webpack hash in the static chunks to reflect recent changes.
    - Added meta tags for SEO in the delete account and privacy policy HTML pages.

--- .next-dev/static/chunks/webpack.js
+++ .next-dev/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8b9288a30b74965"; }
+/******/ 		__webpack_require__.h = function() { return "c550179e35cf9547"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
